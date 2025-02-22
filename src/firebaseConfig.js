80d831a1020efabbac94593542cb6c60dcfaddbf
fazalshah007 <bilalfazal52@gmail.com>
2025-02-22 10:34:47 +0500

    final stage

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,26 +1,9 @@
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth"
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyD6l0UwbWtP6eD4xKMVYUwXt290NqHCGN4",
-//   authDomain: "todo-app-005.firebaseapp.com",
-//   projectId: "todo-app-005",
-//   storageBucket: "todo-app-005.firebasestorage.app",
-//   messagingSenderId: "717574067527",
-//   appId: "1:717574067527:web:29c0581b65053b6276a862"
-// };
-
-// const app = initializeApp(firebaseConfig);
-// const auth = getAuth(app);
-
-// export { auth };
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBUZqJ3CRX0pPQfNe5EvQTAGYCAe2OwTuE",
+  apiKey: "",
   authDomain: "react-hackathon-05.firebaseapp.com",
   projectId: "react-hackathon-05",
   storageBucket: "react-hackathon-05.firebasestorage.app",
